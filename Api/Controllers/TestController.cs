5048d867604e1ca1b46a870c89d9e16de2d12738
shirokikhdo <shirokikhdo@gmail.com>
2024-12-22 21:45:24 +0300

    Тестовый контроллер

---
+++ Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("test")]
+    public string GetHelloWorldText() => 
+        "Hello world";
+
+    [HttpGet("hello/{name}")]
+    public string GetGreetingByName(string name) =>
+        $"Привет {name}";
+}
