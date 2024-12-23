a6198bfb7833a2ffe34f701973ed334a6fa35ca3
shirokikhdo <shirokikhdo@gmail.com>
2024-12-23 22:43:12 +0300

    Подготовка представления

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <th>Имя 1</th>
+                <th>Email 1</th>
+              </tr>
+              <tr>
+                <th>2</th>
+                <th>Имя 2</th>
+                <th>Email 2</th>
+              </tr>
+              <tr>
+                <th>3</th>
+                <th>Имя 3</th>
+                <th>Email 3</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
