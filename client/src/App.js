95e2fd530adff5c9f95e4b0ec31c6588efb61442
shirokikhdo <shirokikhdo@gmail.com>
2024-12-24 22:36:07 +0300

    Настройка POST—запросов

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,8 @@ const App = () => {
       name: contactName, 
       email: contactEmail
     };
+    const url = `${baseApiUrl}/contacts`;
+    axios.post(url, item);
     setContacts([...contacts, item]);
   }
 
