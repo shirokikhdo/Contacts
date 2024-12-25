e5bbf2438316e2b324a1b8cdad3e9b60c44a3a4d
shirokikhdo <shirokikhdo@gmail.com>
2024-12-25 23:10:23 +0300

    DbContext

---
+++ Api/Storage/SqliteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Storage;
+
+public class SqliteDbContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    public SqliteDbContext(DbContextOptions<SqliteDbContext> options) 
+        : base(options)
+    {
+        
+    }
+}
